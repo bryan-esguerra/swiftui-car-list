9d1b9cd402e168465924f8d779ea2f9ec76fb62b
bryan-esguerra <robert.b.esguerra@gmail.com>
2024-04-16 20:31:12 +0800

    init: created workspace and initial pods

---
+++ car-list-sample/car-list-sample/car_list_sampleApp.swift
@@ -0,0 +1,20 @@
+//
+//  car_list_sampleApp.swift
+//  car-list-sample
+//
+//  Created by Robert Bryan Esguerra on 16/4/24.
+//
+
+import SwiftUI
+
+@main
+struct car_list_sampleApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
